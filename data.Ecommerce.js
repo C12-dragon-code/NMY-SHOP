730711cb691dd7ca54488dfdd8524c6b8ca78e76
malekhizaoui <hizaoui.malek.habib@gmail.com>
2021-05-10 13:06:30 +0100

    doomy data

---
+++ data.Ecommerce.js
@@ -0,0 +1,89 @@
+window.data=[
+    {
+    url:'https://i.pinimg.com/originals/41/3c/2b/413c2b7eff3b3c17a557690d6e3d280a.jpg',
+    name:'Head',
+    description:'head shirt',
+    prices: '20$',
+    view:0
+},
+    {
+    url:'https://sportlandamerican.com/16034-large_default/casquette-nba-cleveland-cavaliers-new-era-classic-team-snapback-9fifty-rouge.jpg',
+    name:'clevland  ',
+    description:'clevland hat nba ',
+    prices:'25$',
+    view:0
+},
+    {
+    url:'https://images-na.ssl-images-amazon.com/images/I/71zfsfFJjjL._AC_UL1500_.jpg',
+    name:'t-shirt',
+    description:'tennis-ball shirt ',
+    prices:'30$',
+    view:0
+},
+    {
+    url:'https://5.imimg.com/data5/SL/TY/IV/SELLER-1756364/t-shirts-custom-design-500x500.jpg',
+    name:'t-shirt',
+    description:'rafa t-shirt',
+    prices:'36$',
+    view:0
+},
+    
+    {
+    url:'https://images-na.ssl-images-amazon.com/images/I/51XwLxY0iYL._AC_UX679_.jpg',
+    name:'Lakers',
+    description:'lakers hat  nba',
+    prices:'70$',
+    view:0
+},
+
+{
+    url:'https://dks.scene7.com/is/image/GolfGalaxy/16HEDATS6STRNGXXXPRQ_Black?qlt=70&wid=600&fmt=pjpeg',
+    name:'racket',
+    description:'wilson',
+    prices:'98$',
+    view:0
+},
+
+{
+    url:'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/124ec4af-5b68-4905-a059-5bf4ebc7e80f/pg-4-basketball-shoe-tszRns.png',
+    name:'Nike',
+    description:'nike shoes',
+    prices:'88$',
+    view:0
+},
+{
+    url:'https://cdn.shopify.com/s/files/1/2660/5202/products/xoir1j2ihw9b4bw2apj5_a6ba22e9-258d-435e-86c6-7fadbe115a84_1400x.jpg?v=1598896506',
+    name:'bed',
+    description:'beige bed',
+    prices:'66$',
+    view:0
+},
+{
+    url:'https://images.samsung.com/is/image/samsung/n-africa-fhdtv-n5300-ua43n5300asxmv-frontblack-116252470?$720_576_PNG$',
+    name:'TV',
+    description:'samsung TV',
+    prices:'90$',
+    view:0
+},
+{
+    url:'https://images-dynamic-arcteryx.imgix.net/S21/1350x1710/Keppel-Trench-Coat-Megacosm.jpg?auto=format&w=1350',
+    name:'coat',
+    description:'blue coat',
+    prices:'98$',
+    view:0
+},
+{
+    url:'https://i.ebayimg.com/images/g/Nr8AAOSwHbJdi3S1/s-l400.jpg',
+    name:'controller',
+    description:'lyon controller for ps4',
+    prices:'97$',
+    view:0
+},
+{
+    url:'https://i.pinimg.com/564x/b3/72/57/b372571832b8bea3e85f30dd749c06c0.jpg',
+    name:'controller',
+    description:'adidas controller for ps4',
+    prices:'90$',
+    view:0
+},
+]
