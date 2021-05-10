745dec77c30a04f668ff99c706c4ae91953c8e9b
Nassir01 <you@example.com>
2021-05-11 00:49:12 +0200

    skeleton of our

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require("body-parser");
 
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'client','dist')));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port, () => {
-    console.log(`listening to the port :${port}`);
+    console.log(`listening to the port http://localhost:${port}`);
 });
