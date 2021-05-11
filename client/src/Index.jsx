3e67f4b96fcc7cbad607a5101ee718ef49f0d8e3
Nassir01 <you@example.com>
2021-05-11 15:58:16 +0200

    details

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -1,22 +1,37 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Pro from './components/Pro.jsx'
+import MoreDetails from './components/more-details.jsx'
 export default class Index extends Component {
     constructor(props) {
         super(props)
          this.state={
-             component:'',
+             view:"pro",
+             product:null,
          }
+         this.ChangeView=this.ChangeView.bind(this)
+         this.renderView=this.renderView.bind(this)
     }
-    // detail(){
-    //     this.setState({
-    //         component:
-    //     })
-    
+    ChangeView(view, product) {
+        this.setState({
+            view: view,
+            product: product
+
+        })
+    }
+    renderView() {
+    const  view = this.state.view;
+    if (view === "pro") {
+        return <Pro ChangeView={this.ChangeView}/>
+    }
+    if (view === "details") {
+        return <MoreDetails product={this.state.product} />
+    }
+    }
     render() {
         return (
             <div>
-              <Pro/>
+              {this.renderView()}
             </div>
         )
     }
